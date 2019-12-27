e52e1ec86f2833e5f0916adfdf7116d2a7ea6c7c
kevbarrios <59294983+kevbarrios@users.noreply.github.com>
2019-12-27 15:32:46 -0600

    Update zbtprinter.js

--- zbtprinter.js
+++ zbtprinter.js
@@ -1,22 +1,10 @@
-/*
- * cordova.plugins.zbtprinter.print = function(str, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'ZebraBluetoothPrinter', 'print', [str]);
-};
-*/
-
-//var ZebraBluetoothPrinterLoader = function (require, exports, module) {
-
-	var exec = require("cordova/exec");
+var exec = require("cordova/exec");
  
- 	function ZebraBluetoothPrinter() {
-    }
+  function ZebraBluetoothPrinter() {}
 
-    ZebraBluetoothPrinter.prototype.esegui = function (successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'ZebraBluetoothPrinter', 'print', ["ciccio", "AC:3F:A4:52:73:C4"]);
-    };
-    var bluetoothPrinter = new ZebraBluetoothPrinter();
-    module.exports = bluetoothPrinter;
-//};
+  ZebraBluetoothPrinter.prototype.esegui = function (successCallback, errorCallback) {
+    exec(successCallback, errorCallback, 'ZebraBluetoothPrinter', 'print', ["ciccio", "AC:3F:A4:52:73:C4"]);
+  };
+  var bluetoothPrinter = new ZebraBluetoothPrinter();
+  module.exports = bluetoothPrinter;
 
-//ZebraBluetoothPrinterLoader(require, exports, module);
-//cordova.define("cordova/plugins/zbtprinter", ZebraBluetoothPrinter);
